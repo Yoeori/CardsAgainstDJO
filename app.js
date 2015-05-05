e1e99ca39768f548d6ae1ca6d1ac64ddd935d501
Yoeri Otten <yoeori@live.nl>
2015-05-05 20:16:57 +0200

    woeps fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use("/js", express.static('client'));
 app.use("/js", express.static('shared'));
 
 server.listen(3003, function() {
-  console.log("Server listening at: *:3000");
+  console.log("Server listening at: *:3003");
 });
 
 var game = Object.create(require("./server/app"));
