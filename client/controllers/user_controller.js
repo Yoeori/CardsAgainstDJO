0587e5badc00fa7d7310a89372056440fa822bad
Yoeri Otten <yoeori@live.nl>
2015-05-15 19:26:29 +0200

    I think I changed something

--- client/controllers/user_controller.js
+++ client/controllers/user_controller.js
@@ -1,3 +1,15 @@
 var UserController = {
-  
+
+  updateUsername: function(app, name) {
+    app.getSocket().emit("user_set_username", name);
+  },
+
+  receiveUsername: function(app, name) {
+    app.getUser().setUsername(name);
+  },
+
+  receiveUserData: function(app, data) {
+
+  }
+
 }
