7891907db5776fc5a480f17db6a1c32f89689850
Jelmerro <bla.dingetje@gmail.com>
2015-05-23 00:09:37 +0200

    Added config check and fixed example

--- app.js
+++ app.js
@@ -6,12 +6,34 @@ var app = express();
 var server = http.createServer(app);
 var io = require("socket.io").listen(server);
 
-if(!fs.existsSync("./server/config.json")) {
+var config;
+
+if(fs.existsSync("./server/config.json")) {
+	config = require("./server/config");
+	if (typeof config["database"]["host"] == 'undefined') {
+		incompleteConfigExit();
+	}
+	if (typeof config["database"]["user"] == 'undefined') {
+		incompleteConfigExit();
+	}
+	if (typeof config["database"]["password"] == 'undefined') {
+		incompleteConfigExit();
+	}
+	if (typeof config["database"]["database"] == 'undefined') {
+		incompleteConfigExit();
+	}
+	if (typeof config["port"] == 'undefined') {
+		incompleteConfigExit();
+	}
+} else {
 	console.error("Error: Configuration file doesn't exist.");
 	process.exit();
 }
 
-var config = require("./server/config");
+function incompleteConfigExit() {
+	console.error("Error: Configuration file is incomplete.");
+	process.exit();
+}
 
 app.use(express.static('static'));
 app.use("/js", express.static('client'));
